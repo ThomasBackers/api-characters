af84b04f485645c0806445f137bc2fc47a6b3ab6
ThomasBackers <thomasbackers@gmail.com>
2021-10-24 01:33:20 +0200

    apiPut done + initializer is online

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,11 @@ import "regenerator-runtime/runtime"
 // VARIABLES
 // --- links ---
 const apiUrl = "https://character-database.becode.xyz/characters/"
+const initializerId = "07380f8d-d601-42a0-acf8-12285b25210d"
 // --- pages content ---
 const homeContent = document.querySelector(".--home")
 const editContent = document.querySelector(".--edit")
+const profileContent = document.querySelector(".--profile")
 // --- buttons ---
 const addButton = document.querySelector(".top-bar__add-btn")
 const searchButton = document.querySelector(".top-bar__search__btn")
@@ -48,6 +50,21 @@ const apiPost = async (url, data) => {
     }
 }
 
+const apiPut = async (url, id, data) => {
+    try {
+        const response = await fetch(url + id, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        console.log(response)
+    } catch(error) {
+        console.log(error)
+    }
+}
+
 /**
  * @param {String} url - link to API
  * @param {String} id - data to delete ID
@@ -68,6 +85,7 @@ const searchButtonEffect = () => {
     if (searchInput.value !== "") {
         if (homeContent.style.display === "none") {
             //editContent.style.display = "none"
+            //profileContent.style.display = "none"
             homeContent.style.display = "initial"
         }
         const purpose = searchInput.value.toLowerCase()
@@ -88,4 +106,9 @@ searchButton.addEventListener("click", searchButtonEffect)
 
 searchInput.addEventListener("keydown", event => {
     if (event.key === "Enter") searchButtonEffect()
+})
+
+apiPut(apiUrl, initializerId, {
+    name: "ft_initializer",
+    map: []
 })
